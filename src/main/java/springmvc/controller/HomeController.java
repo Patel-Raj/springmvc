63f65e283b46955e33977268434804d5ffcc8060
Raj Patel <patelraj22777@gmail.com>
2022-01-13 08:33:35 +0530

    Passing data from Controller to view

--- src/main/java/springmvc/controller/HomeController.java
+++ src/main/java/springmvc/controller/HomeController.java
@@ -1,13 +1,25 @@
 package springmvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.Servlet;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		List<String> friends = new ArrayList<String>();
+		friends.add("Rachit");
+		friends.add("Rohit");
+		
+		model.addAttribute("name", "Raj");
+		model.addAttribute("friends", friends);
 		return "index";
 	}
 
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +8,13 @@
 <title>Habits tracker</title>
 </head>
 <body>
-	<h1> Welcome to Habit tracker !</h1> 
+	<%
+		String name = (String) request.getAttribute("name");
+		
+	%>
+	<h1> Welcome <%=name%> to Habit tracker !</h1> 
 	<h2> Let's build upon good habits and break bad ones. </h2> 
+	
+	<h2>Your friends are already here !!</h2>
 </body>
 </html>
