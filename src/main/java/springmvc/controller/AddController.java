6c526e3458574bd03bb4e10a3ea4ee9b4750634c
Raj Patel <patelraj22777@gmail.com>
2022-01-22 18:20:32 +0530

    added ModelAndView

--- src/main/java/springmvc/controller/AddController.java
+++ src/main/java/springmvc/controller/AddController.java
@@ -1,7 +1,11 @@
 package springmvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AddController {
@@ -12,7 +16,14 @@ public class AddController {
 	}
 	
 	@RequestMapping("/about")
-	public String aboutPage() {
-		return "about";
+	public ModelAndView aboutPage() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("about");
+		modelAndView.addObject("name", "Raj Patel");
+		List<String> friends = new ArrayList<String>();
+		friends.add("Parth");
+		friends.add("Urvish");
+		modelAndView.addObject("friends", friends);
+		return modelAndView;
 	}
 }
--- src/main/webapp/about.jsp
+++ src/main/webapp/about.jsp
@@ -1,3 +1,5 @@
+<%@page import="java.util.ArrayList"%>
+<%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -8,5 +10,17 @@
 </head>
 <body>
 	This is about page.
+	<%
+		String name = (String) request.getAttribute("name");
+		List<String> friends = (ArrayList) request.getAttribute("friends");
+	%>
+	<h1>My name is : <%= name %></h1>
+	<%
+		for(String n : friends) {
+	%>
+		<h1>Friend : <%=n %></h1>	
+	<% 
+		}
+	%>	
 </body>
 </html>
