61833f1cad0c6ed1f250d0af2ee4276bbfa6e84b
Raj Patel <patelraj22777@gmail.com>
2022-01-22 18:43:42 +0530

    RequestMapping on class

--- src/main/java/springmvc/controller/AddController.java
+++ src/main/java/springmvc/controller/AddController.java
@@ -5,17 +5,19 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/first")
 public class AddController {
 
-	@RequestMapping("/add")
+	@RequestMapping(path="/add", method=RequestMethod.GET)
 	public String add() {
 		return "result";
 	}
 	
-	@RequestMapping("/about")
+	@RequestMapping(path="/about", method=RequestMethod.GET)
 	public ModelAndView aboutPage() {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("about");
